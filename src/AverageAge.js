142e909f37338b4954f85068d6fea3becc408a44
Alizadehf-laya <alizadehlayaf@gmail.com>
2024-10-29 09:50:16 +0330

    both admins and user averageage  shown

--- src/AverageAge.js
+++ src/AverageAge.js
@@ -6,13 +6,16 @@ const AverageAge = () => {
   const userU=User.filter((user)=>user.role ==="user")  
 
 const total=userU.reduce((total,user) =>total +user.age,0);
+const totalA=userA.reduce((total,user) =>total +user.age,0);
 const average =total /userU.length;
+const AverageAge =total /userA.length;
   return (
     <>
       {/* {userA.map(createCardAge)} */}
       <div className="break"></div>
       <div className="card">
     میانگین سن یوزرها:‌ <span data-testid="average-age">{average}</span>
+    میانگین سن ادمین ها:‌ <span data-testid="average-age">{AverageAge}</span>
       </div>
     </>
   )
